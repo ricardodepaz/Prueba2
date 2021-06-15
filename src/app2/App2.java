471912c6a02a60c3593d8c44c6f0c01a3da0b022
dn192246 <ricar.arturo@gmail.com>
2021-06-15 08:54:16 -0600

    Creé un formulario con un botón que permite saludar en consola

---
+++ src/app2/App2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app2;
+
+/**
+ *
+ * @author HP
+ */
+public class App2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void saludar(){
+        System.out.println("Hola a todos");
+    }
+    
+}
